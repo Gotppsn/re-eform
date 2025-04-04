3de3658f94d6f60fcc2322325783efb825121453
Got Panupol <126560479+Gotppsn@users.noreply.github.com>
2025-04-04 17:24:21 +0700

    update

--- obj/Debug/net9.0/EFormBuilder.AssemblyInfo.cs
+++ obj/Debug/net9.0/EFormBuilder.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EFormBuilder")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f4317a6a340688f4e7fb4bdde0d89b814c30017")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+390c7fbb285bfe94077587f313c584ded60684b7")]
 [assembly: System.Reflection.AssemblyProductAttribute("EFormBuilder")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EFormBuilder")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
